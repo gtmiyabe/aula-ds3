e0bd3c629d06f28fcfaa352818539ebb2db12f10
gtmiyabe <gtmiyabe@gmail.com>
2019-06-09 23:55:16 -0300

    added axios and updated endpoint to post data for employee creation

--- frontend/src/components/employee/EmployeeContainer.js
+++ frontend/src/components/employee/EmployeeContainer.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import { Form } from "antd";
+import axios from "axios";
+import { get } from "lodash";
+import { Form, message } from "antd";
+
 import AddEmployee from "./AddEmployee";
 
 class EmployeeContainer extends Component {
@@ -9,7 +12,17 @@ class EmployeeContainer extends Component {
     e.preventDefault();
     this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
-        console.log("values test submit: ", values);
+        // const loading = message.loading("Saving employee", 0);
+        axios
+          .post("/employee", { ...values })
+          .then(res =>
+            message.success(
+              `Employee ${get(res, ["data", "name"], "")} successfully saved.`,
+              3
+            )
+          )
+          .catch(() => message.error("Error.", 3));
+        // .finally(() => loading());
       }
     });
   };
